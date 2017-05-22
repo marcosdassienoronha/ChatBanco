a51047de4fc96d495d668da2a5619786f8cf486a
marcosdassienoronha <marcosdassienoronha@gmail.com>
2017-05-22 14:25:50 -0300

    Versão 1.0

---
+++ formflow/Controllers/MessagesController.cs
@@ -0,0 +1,87 @@
+﻿
+using ChatBot.Serialization;
+using ChatBot.Services;
+using formflow.FormFlow;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Connector;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+
+
+namespace formflow
+{
+    //[BotAuthentication]
+    public class MessagesController : ApiController
+    {
+
+
+        [ResponseType(typeof(void))]
+
+
+
+        public virtual async Task<HttpResponseMessage> Post([FromBody] Activity activity)          //utilizar async habilita a utilizacao de await dentro do metodo | Task é
+                                                                                                   //uma unidade de trabalho que devera ser executada, ao passar uma task em
+                                                                                                   //um await, voce passa a executar esta tarefa em uma thread separada. | await é o gerenciador de thread que recebe como parametro uma task
+        {
+            string sim = string.Empty;
+            string nao = string.Empty;
+            var intent = new Intent();
+            var entity = new ChatBot.Serialization.Entity();
+            var response = await Luis.GetResponse(activity.Text);
+
+            var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+            //Método Start
+            //var resposta = await Start(activity);
+            //var msg = activity.CreateReply(resposta, "pt-BR");
+            //await connector.Conversations.SendToConversationAsync(msg);
+
+            //Método Cases
+
+
+
+            if (activity != null && activity.GetActivityType() == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity, () => {
+                    return Chain.From(() => FormDialog.FromForm(Enquiry.BuildForm));         //O metodo Chain.From faz com que construa um dialogo que vai fazer copia de outro dialogo quando iniciado, por isso precisa de uma dialogo como parametro
+                });
+
+
+                foreach (var item in response.entities)
+                {
+                    switch (item.type)                                                       //Resposta já vinculada ao LUIS, ou seja, detecta a intenção do usuario, se era SIM ou NAO que o mesmo queria dizer.
+                    {
+                        case "sim":                                                          //caso a entidade seja SIM ou uma de suas Intenções
+                            sim = item.entity;
+                            break;
+                        case "nao":                                                          //caso a entidade seja NAO ou uma de suas Intenções
+                            nao = item.entity;
+                            break;
+                    }
+                   
+
+                }
+
+
+               
+            }
+
+
+            var responseHttp = Request.CreateResponse(HttpStatusCode.OK);
+            return responseHttp;
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ formflow/FormFlow/Enquiry.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Bot.Builder.FormFlow;
+using System;
+
+
+
+
+namespace formflow.FormFlow
+{
+    public enum YesOrNoOptions { Sim = 1, Nao };
+
+    [Serializable]
+    public class Enquiry
+    {
+
+
+        //[Prompt("Qual é o seu nome?")]
+        // public string Name { get; set; }
+        [Prompt("Olá, sou um representante do Banco 1500, notamos que há algumas contas suas pendentes e gostariamos de negocia-las. A conta número 23323-90 é " +
+            "pertencente a você? {||}")]
+        public YesOrNoOptions Fala1 { get; set; }
+        [Prompt("Voce está de acordo com o combinado na negociação? {||}")]
+         public bool Fala2 { get; set; }
+
+
+        public Service ServiceRequired { get; set; }
+
+        public enum Service
+        {
+            Consultancy, Support, ProjectDelivery, Unknown
+        }
+        public static IForm<Enquiry> BuildForm()
+        {
+            var builder = new FormBuilder<Enquiry>();
+            builder.Configuration.DefaultPrompt.ChoiceStyle = ChoiceStyleOptions.Auto;
+            builder.Configuration.Yes = new string[] { "sim" };
+            builder.Configuration.No = new string[] { "nao" };
+            // Builds an IForm<T> based on BasicForm
+            return builder.Build();
+        }
+
+        public static IFormDialog<Enquiry> BuildFormDialog(FormOptions options = FormOptions.PromptInStart)
+        {
+            // Generated a new FormDialog<T> based on IForm<BasicForm>
+            return FormDialog.FromForm(BuildForm, options);
+        }
+
+
+        //  public static IForm<Enquiry> BuildEnquiryForm()                                 //Cria um formulario adicionando alguns campos, retorna um IForm
+        //  {
+        //      return new FormBuilder<Enquiry>()                                           //aqui se define a ordem da apresentação das mensagens (Bot-to-User)
+        //          .Field("Fala1")
+        //          .Field("Name")
+        //          .Build();
+        //  }
+
+
+    }
+}
